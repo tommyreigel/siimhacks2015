465bb093fdc37cfaffea8c8625936cf0a66ac603
Tommy Reigel <treigel@analytical.info>
2015-05-29 23:06:26 -0400

    initial functionalty; pull specialties, template titles, and report templates from http://www.radreport.org/

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,22 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('http://127.0.0.1:1337/json/specialty/').success(function(data) {
+    $scope.specialties = data.response.specialties;
+  });
 
+  $scope.updateTemplates = function(){
+    $http.get('http://127.0.0.1:1337/json/?specialty=' + $scope.selectedSpecialtyId).success(function(data) {
+      $scope.templates = data.response.template
+    })
+  };
+
+  $scope.updateSelectedTemplate = function(){
+    $http.get('http://127.0.0.1:1337/json/template/?id=' + $scope.selectedTemplateId).success(function(data) {
+      $("#templateIframe").empty()
+      $("#templateIframe").append(data)
+      $scope.selectedTemplate = _.first(data.response.template)
+    })
+  }
 }]);
